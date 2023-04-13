fd7d6b0eba2496110028881ef90285e6e7387e8f
yyorange <1258720926@qq.com>
2023-04-13 11:01:53 +0800

    finish p0

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,15 @@ class Module:
 
     def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = True
+        for module in self._modules.keys():
+            self._modules[module].train()
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        self.training = False
+        for moudule in self._modules.keys():
+            self._modules[moudule].eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
@@ -47,13 +49,26 @@ class Module:
         Returns:
             The name and `Parameter` of each ancestor parameter.
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        parameters = []
+        for item in self._parameters.items():
+            parameters.append(item)
+        for name, module in self._modules.items():
+            for param_name, param in module.named_parameters():
+                new_name = name + "." + param_name
+                parameters.append((new_name, param))
+        return parameters
+
 
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError("Need to implement for Task 0.4")
+        parameters = []
+        for parameter in self._parameters.values():
+            parameters.append(parameter)
+
+        for module in self._modules.values():
+            parameters.extend(module.parameters())
+
+        return parameters
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -27,6 +27,7 @@ from minitorch.operators import (
 
 from .strategies import assert_close, small_floats
 
+
 # ## Task 0.1 Basic hypothesis tests.
 
 
@@ -70,8 +71,8 @@ def test_id(a: float) -> None:
 @given(small_floats)
 def test_lt(a: float) -> None:
     "Check that a - 1.0 is always less than a"
-    assert lt(a - 1.0, a) == 1.0
-    assert lt(a, a - 1.0) == 0.0
+    assert lt(a - 1.0, a) == 1
+    assert lt(a, a - 1.0) == 0
 
 
 @pytest.mark.task0_1
@@ -86,9 +87,9 @@ def test_max(a: float) -> None:
 @pytest.mark.task0_1
 @given(small_floats)
 def test_eq(a: float) -> None:
-    assert eq(a, a) == 1.0
-    assert eq(a, a - 1.0) == 0.0
-    assert eq(a, a + 1.0) == 0.0
+    assert eq(a, a) == 1
+    assert eq(a, a - 1.0) == 0
+    assert eq(a, a + 1.0) == 0
 
 
 # ## Task 0.2 - Property Testing
@@ -99,53 +100,58 @@ def test_eq(a: float) -> None:
 
 
 @pytest.mark.task0_2
-@given(small_floats)
-def test_sigmoid(a: float) -> None:
+@given(small_floats, small_floats)
+def test_sigmoid(a: float, b: float) -> None:
     """Check properties of the sigmoid function, specifically
     * It is always between 0.0 and 1.0.
     * one minus sigmoid is the same as sigmoid of the negative
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 1.0 >= sigmoid(a) >= 0.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert_close(sigmoid(0.0), 0.5)
+    assert a == b or (a < b and sigmoid(a) <= sigmoid(b)) or (a > b and sigmoid(a) >= sigmoid(b))
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a < b < c:
+        assert a < c
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(a, b) == mul(b, a)
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, add(b, c)), add(mul(a, b), mul(a, c)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats, small_floats)
+def test_other(a: float, b: float) -> None:
     """
     Write a test that ensures some other property holds for your functions.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if a > 1e-6:
+        assert_close(mul(a, inv(a)), 1.0)
+    assert_close(neg(neg(b)), b)
+    assert_close(add(add(a, b), - b), a)
 
 
 # ## Task 0.3  - Higher-order functions
@@ -173,8 +179,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    assert_close(sum(ls1) + sum(ls2), sum(addLists(ls1,ls2)))
 
 
 @pytest.mark.task0_3
@@ -215,7 +220,7 @@ def test_one_args(fn: Tuple[str, Callable[[float], float]], t1: float) -> None:
 @given(small_floats, small_floats)
 @pytest.mark.parametrize("fn", two_arg)
 def test_two_args(
-    fn: Tuple[str, Callable[[float, float], float]], t1: float, t2: float
+        fn: Tuple[str, Callable[[float, float], float]], t1: float, t2: float
 ) -> None:
     name, base_fn = fn
     base_fn(t1, t2)
